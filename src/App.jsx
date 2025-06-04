047e1ee181a35248c99c762e8ec2791690e6b398
yuricifiello <yuricifiello@gmail.com>
2025-06-04 22:03:39 +0200

    fix app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import Header from "./components/layouts/Header";
-import Main from "./components/layouts/Main";
-import Footer from "./components/layouts/Footer";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { PostsProvider } from "./contexts/PostsContext";
+import PostsPage from "./pages/PostsPage";
+import PostDetail from "./pages/PostDetail";
 
 export default function App() {
   return (
-    <>
-      <Header />
-      <Main />
-      <Footer />
-    </>
+    <BrowserRouter>
+      <PostsProvider>
+        <Routes>
+          <Route path="/" element={<PostsPage />} />
+          <Route path="/posts/:id" element={<PostDetail />} />
+        </Routes>
+      </PostsProvider>
+    </BrowserRouter>
   );
 }
