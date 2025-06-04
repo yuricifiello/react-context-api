e349645ec46ef83959537bd468d7b2e4f5ef63e9
yuricifiello <yuricifiello@gmail.com>
2025-06-04 22:04:06 +0200

    add postpage

---
+++ src/pages/PostsPage.jsx
@@ -0,0 +1,10 @@
+import PostsList from "../components/PostsList";
+
+export default function PostsPage() {
+  return (
+    <div className="container">
+      <h1>Lista dei Post</h1>
+      <PostsList />
+    </div>
+  );
+}
